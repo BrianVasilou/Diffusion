b2efd2939b346d61e78d0c0600fe51963cbc6acd
BrianVasilou <40274020+BrianVasilou@users.noreply.github.com>
2018-06-17 17:15:45 -0400

    Update simple_diffusion_example.m

--- simple_diffusion_example.m
+++ simple_diffusion_example.m
@@ -1,46 +1,45 @@
 clc
-clear
-
 %define all variables/constants
 
-N=100;
-sigma_a = 0.5;
+N=100; %number of cells
+sigma_a = 0.5; 
 S = 6*ones(N,1);
 D = 1/3;
-x = linspace(0,5,100);
+R = 1000; %right boundary value
+L = 0; %left boundary value
+slab_length = R-L;
+delta_x = slab_length/N;
+x = linspace(0+delta_x/2,slab_length-delta_x/2,N); %needed to shift by delta_x/2 because 1 and 0 are edges, not node centers
 
 %create a sparse 100x100 matrix
-
 cm = sparse(N,N);
 
 %interior nodes
-
 for i = 2:N-1
-    
-    delta_x = 5;  % unsure on this, my instinct says it should be the delta 
-                  % x, or difference between nodes, which is 0.0505, but 
-                  % that does not give the correct answer
-                  
+              
     cm(i,i-1) = ((-D)./(delta_x.^2));
     cm(i,i) = ((D./(delta_x.^2))+(D./(delta_x.^2))+sigma_a);
     cm(i,i+1) = ((-D)./(delta_x.^2));  
     
 end
 
+%FLUX = 0 AT BOTH BOUNDS
+
 %Left Bound
 i = 1;
-cm(i,i) = ((D./(delta_x.^2))+sigma_a);
-
+cm(i,i) = (D./(delta_x.^2))+(2*D./(delta_x.^2))+sigma_a;
+cm(i,i+1)= (-D./delta_x.^2);
 
 %Right Bound
 i = N;
-cm(i,i) = ((D./(delta_x.^2))+sigma_a);
+cm(i,i) = (D./(delta_x.^2))+(2*D./(delta_x.^2))+sigma_a;
+cm(i,i-1) = (-D./delta_x.^2);
 
 
+phi = cm\S;
+plot(phi)
+hold on
 
-phi = cm\S
-plot(x,phi)
-ylim([0,20])
-xlim([-1,6])
+
 
 
